16f8511df6164bc5c2dbd5e0dc119ef1957c8206
VanesaMartos <97459480+VanesaMartos@users.noreply.github.com>
2022-12-04 17:55:01 +0100

    js code (#3)

---
+++ Display.js
@@ -0,0 +1,56 @@
+class Display {
+  constructor(displayPrevious, displayActual) {
+    this.displayActual = displayActual;
+    this.displayPrevious = displayPrevious;
+    this.calculator = new Calculator();
+    this.operatorType = undefined;
+    this.actual = "";
+    this.previous = "";
+    this.symbol = {
+      add: "+",
+      substract: "-",
+      multiply: "x",
+      divide: "/",
+    }
+  }
+
+  delete() {
+    this.actual = this.actual.toString().slice(0, -1);
+    this.printValue();
+  }
+
+  deleteAll() {
+    this.actual = "";
+    this.previous = "";
+    this.operatorType = undefined;
+    this.printValue();
+  }
+
+  compute(type) {
+    this.operatorType !== "equal" && this.calculate();
+    this.operatorType = type;
+    this.previous = this.actual || this.previous;
+    this.actual = "";
+    this.printValue();
+  }
+
+  addNumber(number) {
+    if (number === "." && this.actual.includes(".")) return
+    this.actual = this.actual.toString() + number.toString();
+    this.printValue();
+  }
+
+  printValue() {
+    this.displayActual.textContent = this.actual;
+    this.displayPrevious.textContent = `${this.previous} ${this.symbol[this.operatorType] || ""}`;
+  }
+
+  calculate() {
+    const previous = parseFloat(this.previous);
+    const actual = parseFloat(this.actual);
+
+    if (isNaN(actual) || isNaN(previous) ) return
+    this.actual = this.calculator[this.operatorType](previous, actual);
+  }
+
+  }
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Adios mundo')
+const displayPrevious = document.getElementById("previous");
+const displayActual = document.getElementById("actual");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+
+const display = new Display(displayPrevious, displayActual);
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => display.addNumber(button.innerHTML));
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener("click", () => display.compute(button.value))
+})
